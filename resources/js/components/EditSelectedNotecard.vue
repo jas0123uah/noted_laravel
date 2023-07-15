a42398e2f8e9502a259bd4bf03ad1a558dd6d2ce
jas0123uah <37711124+jas0123uah@users.noreply.github.com>
2023-07-15 10:45:45 -0400

    Add code syntax highlighting to quill

--- resources/js/components/EditSelectedNotecard.vue
+++ resources/js/components/EditSelectedNotecard.vue
@@ -1,32 +1,58 @@
 <template>
-    <div class="d-flex gap-5">
-        <div>
-            <quill-editor :toolbar="[
-
-                ['bold', 'italic', 'underline', 'code-block'], 
-                [{'list': 'ordered'}]
-                [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-                [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-            ]" placeholder="Front..." contentType="html" v-model:content="notecard.front" theme="snow"></quill-editor>
+    <div :class="{'pb-4': add_padding}" class="d-flex gap-5">
+        <div style="max-width: calc(50vw - 3rem);">
+            <quill-editor :options="options"  :syntax-highlight="customSyntaxHighlight" placeholder="Front..." contentType="html" v-model:content="notecard.front" theme="snow"></quill-editor>
         </div>
-        <div>
+        <div style="max-width: calc(50vw - 3rem);">
             <quill-editor
-            :toolbar="[
-
-                ['bold', 'italic', 'underline', 'code-block'], 
-                [{'list': 'ordered'}]
-                [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-                [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-            ]"
+            :options="options"
             placeholder="Back..." contentType="html" v-model:content="notecard.back" theme="snow"></quill-editor>
         </div>
     </div>
 </template>
 
 <script>
+import hljs from 'highlight.js'
+import 'highlight.js/styles/default.css'; // Import the desired code highlighting style
+import 'highlight.js/styles/monokai-sublime.css'
+
+hljs.configure({   // optionally configure hljs
+    languages: ['javascript', 'ruby', 'python']
+});
+
 export default {
-    props: ['notecard'],
+    props: ['notecard', 'add_padding'],
+    
+    methods: {
+        customSyntaxHighlight(code, lang) {
+            const highlightedCode = hljs.highlight(lang, code).value;
+            return highlightedCode;
+        }
+    },
+    data(){
+        return {
+            options: {
+                debug: 'info',
+                modules: {
+                    toolbar: [
+
+                        ['bold', 'italic', 'underline', 'code-block'], 
+                        [{'list': 'ordered'}]
+                        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+                        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+                    ],
+                    syntax: {
+                        highlight: text => hljs.highlightAuto(text).value
+
+                    }
+                },
+                theme: 'snow'
+            }
+        }
+
+    },
+    
+
 };
 </script>

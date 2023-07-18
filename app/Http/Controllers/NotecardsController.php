266a6b0f56e98ba5697152106499b59c1bdcada3
jas0123uah <37711124+jas0123uah@users.noreply.github.com>
2023-07-18 12:58:14 -0400

    no default

--- app/Http/Controllers/NotecardsController.php
+++ app/Http/Controllers/NotecardsController.php
@@ -25,6 +25,7 @@ class NotecardsController extends Controller
             'stack_id' => $validated_data["stack_id"],
             'user_id' => Auth::id(),
             'repetition' => 0,
+            'next_repetition' => now() ->addDays(1)->startOfDay()
         ]);
 
         return response()->json([
